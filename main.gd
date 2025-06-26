df4641d319d6e1db9d61734305a3206663f75bee
Jarret Diep <jarrettcdi@gmail.com>
2025-06-26 15:49:38 -0600

    add hud functionality

--- main.gd
+++ main.gd
@@ -2,13 +2,11 @@ extends Node
 
 @export var mob_scene: PackedScene;
 
-var score;
+var score = 0;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#new_game();
 	pass;
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -17,14 +15,18 @@ func _process(delta):
 func game_over():
 	$ScoreTimer.stop();
 	$MobTimer.stop();
+	$HUD.show_game_over();
 
 func new_game():
+	$HUD.update_score(score);
+	$HUD.show_message("Get Ready")
 	score = 0;
 	$Player.start($StartPosition.position);
 	$StartTimer.start();
 
 func _on_score_timer_timeout():
 	score += 1;
+	$HUD.update_score(score);
 
 func _on_start_timer_timeout():
 	$MobTimer.start();

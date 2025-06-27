531632dc68c68bf4cbb6f8d160de7c60969d120b
Jarret Diep <jarrettcdi@gmail.com>
2025-06-26 22:12:14 -0600

    updated t godot 4.4, add glue

--- main.gd
+++ main.gd
@@ -16,6 +16,8 @@ func game_over():
 	$ScoreTimer.stop();
 	$MobTimer.stop();
 	$HUD.show_game_over();
+	$Music.stop();
+	$DeathSound.play();
 
 func new_game():
 	$HUD.update_score(score);
@@ -23,6 +25,8 @@ func new_game():
 	score = 0;
 	$Player.start($StartPosition.position);
 	$StartTimer.start();
+	get_tree().call_group("mobs", "queue_free");
+	$Music.play();
 
 func _on_score_timer_timeout():
 	score += 1;
